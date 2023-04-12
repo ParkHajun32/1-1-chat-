b681a6428bc8ad1628c282b4b40fe6b0cf905352
ParkHajun32 <hajun0302@naver.com>
2023-04-12 23:16:35 +0900

    first commit

---
+++ src/js/chat.js
@@ -0,0 +1,42 @@
+const socket = io();
+const nickname = document.querySelector("#nickname")
+const chatlist = document.querySelector(".chatting-list")
+const chatInput = document.querySelector(".chatting-input");
+const sendButton = document.querySelector(".send-button");
+
+sendButton.addEventListener("click",()=>{
+
+    const param = {
+        name: nickname.value,
+        msg: chatInput.value
+    }
+    socket.emit("chatting",param)
+})
+
+
+
+socket.on("chatting",(data)=>{
+    console.log(data)
+    const{name, msg, time} = data;
+    const item = new LiModel(name,msg,item); 
+    item.makeLi()
+})
+
+function LiModel(name,msg,time){
+    this.name = name;
+    this.msg = msg;
+    this.time = time;
+
+    this.makeLi = ()=>{
+        const li = document.createElement("li");
+        li.classList.add(nickname.value === this.name ? "sent":"received")
+        const dom = ` <span class="profile">
+        <span class="user">${this.name}</span>
+        <img class ="image" src = "https://placeimg.com/200/50/any"alt="any">
+    </span>
+    <span class="message">${this.msg}</span>
+    <span class="time">${this.time}</span>`;
+    li.innerHTML = dom;
+    chatList.appendChild(li)
+}
+}
